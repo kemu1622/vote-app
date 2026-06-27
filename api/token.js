7afa2df90b78647937572f0ac62f53006b2494ae
kemu1622 <1659253985@qq.com>
2026-06-27 20:19:37 +0800

    fix: 修复token API

--- api/token.js
+++ api/token.js
@@ -1,18 +1,13 @@
-const crypto = require('crypto');
+const { randomBytes } = require('crypto');
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
   
-  if (req.method === 'GET') {
-    const token = crypto.randomBytes(16).toString('hex');
-    return res.status(200).json({ token });
-  }
-  
-  return res.status(405).json({ error: 'Method not allowed' });
+  const token = randomBytes(16).toString('hex');
+  return res.status(200).json({ token });
 };
